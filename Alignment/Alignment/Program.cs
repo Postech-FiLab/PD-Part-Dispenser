830caa6112ba23fa574be26551e430c632a451c5
CHO-JUHYUN <92797862+CHO-JUHYUN@users.noreply.github.com>
2022-06-13 15:03:36 +0900

    Add files via upload

---
+++ Alignment/Alignment/Program.cs
@@ -0,0 +1,55 @@
+﻿using Alignment.MXP_MOTOR;
+using System;
+using System.Threading;
+
+
+namespace Alignment
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            float home_positon;
+
+            Motor motor = new Motor();
+            motor.connect();
+            motor.Servo_Power(false); // false가 항상 모터 on임 
+
+            
+            Read_Optic_Sensor optic_sensor = new Read_Optic_Sensor();
+            int[] input_numbers = new int[] { 11, 15, 13 } ;
+            int[] enable = new int[] {1,1,1};
+      
+            while (true)
+            {
+                string[] result = optic_sensor.Read(input_numbers, enable);
+                if (result[0].Equals("1") && result[1].Equals("1") && result[2].Equals("1")) // 처음에 이미 정렬이 되어 있는 경우 
+                {
+                    
+                    Console.WriteLine(result[0] + "," + result[1] + "," + result[2]);
+                    Console.WriteLine("정렬 끝");
+                    Single home_position = motor.Now_Actual_Position();
+                    Console.WriteLine("Home Position은 " + home_position + " 로 설정이 되었습니다");
+                    Console.ReadKey();
+                    break;
+                }
+                // 정렬이 되어 있지 않은 경우
+
+                motor.position_calibration = 0.5f;
+                motor.Rel_PMove(1);
+                Console.WriteLine(result[0] + "," + result[1] + "," + result[2]);
+                Thread.Sleep(1000);
+
+            }
+
+           
+            // 약간 비틀려져 있는 경우면 어떻게 할까? 
+        }
+
+       
+
+
+    }
+}
---
+++ Alignment/Alignment/Read_Optic_Sensor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alignment
+{
+    public class Read_Optic_Sensor
+    {
+
+        private Byte[] result;
+        private string[] str_result = new string[3] { "0", "0", "0" } ;
+        private int source_number = 1;
+        private int[] input_numbers;
+        private int[] enable;
+
+        public Byte[] Result
+        {
+            get { return this.result; }
+            set { this.result = value; }
+        }
+
+        public int[] Enable
+        {
+            get { return this.enable; }
+            set { this.enable = value; }
+        }
+
+        public int[] InputNum
+        {
+            get { return this.input_numbers; }
+            set { this.input_numbers = value; }
+        }
+
+
+        public string[] Read( int[] input_numbers, int[] enable)
+        {
+            for (int i = 0; i < input_numbers.Length; i++){
+                MXP.MXP_READDIGITALINPUT_IN In;
+                MXP.MXP_READDIGITALINPUT_OUT Out;
+                Byte Result;
+                In.Input.SourceNo = 1;
+                In.InputNumber = input_numbers[i];
+                //Console.WriteLine(input_numbers[i]);
+                //Console.WriteLine(enable[i]);
+                In.Enable = Convert.ToByte(enable[i]);
+                MXP.MXP_ReadDigitalInput(ref In, out Out);
+                Result = Out.Value;
+                //Console.WriteLine(Result.ToString());
+                str_result[i] = Result.ToString();
+            }
+            return str_result;
+        }
+
+
+
+
+
+
+    }
+}
