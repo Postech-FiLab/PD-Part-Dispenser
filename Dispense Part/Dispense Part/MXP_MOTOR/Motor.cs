dd55638b8e6169b26c84e5f1e94a544bfcc16c54
CHO-JUHYUN <92797862+CHO-JUHYUN@users.noreply.github.com>
2022-06-13 15:05:41 +0900

    Add files via upload

---
+++ Dispense Part/Dispense Part/MXP_MOTOR/Motor.cs
@@ -0,0 +1,274 @@
+﻿using Dispense_Part;
+using System;
+
+using System.Threading;
+
+namespace Dispense_Part
+{
+    public class Motor
+    {
+        /// <summary>
+        /// 모션 블락 인덱스 번호 
+        /// 아직 무엇인지 모름.. 
+        /// </summary>
+        public enum MXP_MotionBlockIndex
+        {
+            mcPower = 0,
+            mcReset,
+            mcStop,
+            mcHalt,
+            mcHome,
+            mcMoveAbsolute,
+            mcMoveRelative,
+            mcMoveVelocity,
+            mcMoveLinearAbsolute,
+            mcMoveLinearRelative,
+            mcMoveCircularAbsolute,
+            mcMoveCircularRelative,
+            mcGroupStop,
+            mcWriteParameter,
+            mcWriteBoolParameter,
+            mcET_ReadParameter,
+            mcET_WriteParameter,
+            mcWriteOutputs,
+            mcWriteDigitalOutputs,
+            mcGearIn,
+            mcGearOut,
+            mcGearInPos,
+            mcCamIn,
+            mcCamOut,
+            mcCamTableSelect,
+            mcSetTouchProbe,
+            mcDirectTorqueControl
+        }
+
+        /// <summary>
+        /// MXP Connect 하는 함수.
+        /// Kernel 초기화 --> MXP_InitKernel()
+        /// 커널 동작 시킴 --> MXP_SystemRun()
+        /// 프로그램을 시작하기전 커널 초기화, 커널 동작순으로 무조건 선행이 되어야 한다.
+        /// </summary>
+        public void connect()
+        {
+            UInt32 Status = 0;
+            UInt32 Connection_Check = 0;
+            if (MXP.MXP_InitKernel(ref Status) != MXP.MXP_ret.RET_NO_ERROR)
+            {
+                Console.WriteLine("Mxp Init Kernel fail");
+            }
+            else
+            {
+                if (MXP.MXP_SystemRun() == 0)
+                {
+                    Console.WriteLine("MXP Sysetm Run and Connected");
+                }
+
+            }
+
+            // STATE-OP( 통신 준비 완료 ) 상태까지는 수초의 시간이 소요되므로 
+            // 네트워크 정상상태를 체크 하기 위해서는 아래와 같이 응용이 필요합니다.
+            // While문 돌면서 State 값이 NET_STATE_OP 값과 같아짐.
+            while (Connection_Check == 0)
+            {
+                UInt32 State = 0;
+                UInt32 SlaveNo = 0;
+                MXP.MXP_IsSlaveOnline(SlaveNo, out State); // 개별 slave의 네트워크 상태를 반환함.
+                if (State == (UInt32)MXP.MXP_ONLINESTATE_ENUM.NET_STATE_OP)
+                {
+                    Connection_Check = 1;
+                    Console.WriteLine("Operated");
+                    break;
+
+                }
+
+            }
+        }
+        /// <summary>
+        /// MaxAxis 왜 필요한지 아직 모름.
+        /// </summary>
+        static UInt32 MaxAxis = 0;
+        public static UInt32 IndexCal(UInt32 Index)
+        {
+            return Index * MaxAxis;
+        }
+
+        /// < SUMMARY >
+        ///  * Servo on/off 상태를 제어하는 함수.
+        ///  * 인자 servoonoff : false --> Servo on 
+        ///  
+        /// </SUMMARY>
+        /// 
+        public void Servo_Power(bool servoonoff)
+
+        {
+            MXP.MXP_POWER_IN Pow_In = new MXP.MXP_POWER_IN { };
+            if (!servoonoff)
+            {
+                Pow_In.Axis.AxisNo = 0;
+                Pow_In.Enable = 1;
+                Int32 Ret = MXP.MXP_PowerCmd(0, ref Pow_In);
+                if (Ret == MXP.MXP_ret.RET_NO_ERROR)
+                {
+                    servoonoff = true;
+                    Console.WriteLine("Servo Power on");
+                }
+            }
+            else
+            {
+                Pow_In.Axis.AxisNo = 0;
+                Pow_In.Enable = 0;
+                Int32 Ret = MXP.MXP_PowerCmd(0, ref Pow_In);
+                if (Ret == MXP.MXP_ret.RET_NO_ERROR)
+                {
+                    servoonoff = false;
+                }
+
+            }
+        }
+
+
+
+        public Single align_position = 3375.415f;
+        public void Align()
+        {
+            // 현재 위치에서 Position에 설정된 절대 위치로 이동합니다.
+            // 
+            MXP.MXP_MOVEABSOLUTE_IN Po_Abs_In = new MXP.MXP_MOVEABSOLUTE_IN { };
+            Po_Abs_In.Axis.AxisNo = 0;
+            Po_Abs_In.ContinuousUpdate = 0;
+            Po_Abs_In.Position = align_position;
+            Po_Abs_In.Velocity = 80;
+            Po_Abs_In.Acceleration = (float)15;
+            Po_Abs_In.Deceleration = (float)15;
+            Po_Abs_In.Jerk = 10000;
+            Po_Abs_In.BufferMode = MXP.MXP_BUFFERMODE_ENUM.MXP_BUFFERED;
+            Po_Abs_In.Execute = 0;
+            Int32 Status = MXP.MXP_MoveAbsoluteCmd(110, ref Po_Abs_In);
+            Thread.Sleep(1000);
+            Po_Abs_In.Execute = 1;
+            Status = MXP.MXP_MoveAbsoluteCmd(110, ref Po_Abs_In);
+            Thread.Sleep(8000);
+
+            // 80속도, 가속도 15일때 330도 돌려면 한 8초 정도 줘야함.( 딱 적당 ) 
+        }
+
+        /// 30도 회전을 위한 position (distance) 값:    46.91666667
+        public float position_calibration = 46.875f;
+        /// <summary>
+        /// Rel_PMove()
+        /// Positive 방향으로 30도 회전하는 함수 
+        /// </summary>
+        public void Rel_PMove(int index)
+        {
+            MXP.MXP_MOVERELATIVE_IN Po_Rel_In;
+            Po_Rel_In.Axis.AxisNo = 0;
+            Po_Rel_In.ContinuousUpdate = 0;
+            Po_Rel_In.Distance = position_calibration * index; //= 30;
+            Po_Rel_In.Velocity = 35;
+            Po_Rel_In.Acceleration = (float)10;
+            Po_Rel_In.Deceleration = (float)10;
+            Po_Rel_In.Jerk = 10000;
+            Po_Rel_In.BufferMode = MXP.MXP_BUFFERMODE_ENUM.MXP_BUFFERED;
+            Po_Rel_In.Execute = 0;
+            //Thread.Sleep(500);
+            Int32 Status = MXP.MXP_MoveRelativeCmd(110, ref Po_Rel_In);
+            Thread.Sleep(1000);
+            Po_Rel_In.Execute = 1;
+            Status = MXP.MXP_MoveRelativeCmd(110, ref Po_Rel_In);
+            Thread.Sleep(500);
+        }
+        /// <summary>
+        /// Rel_NMove() 
+        /// Negative 방향으로 30도 회전
+        /// </summary>
+        public void Rel_NMove(int index)
+        {
+            MXP.MXP_MOVERELATIVE_IN Ne_Rel_In;
+            Ne_Rel_In.Axis.AxisNo = 0;
+            Ne_Rel_In.ContinuousUpdate = 0;
+            Ne_Rel_In.Distance = -1 * position_calibration * index;
+            Ne_Rel_In.Velocity = 35;
+            Ne_Rel_In.Acceleration = (float)10;
+            Ne_Rel_In.Deceleration = (float)10;
+            Ne_Rel_In.Jerk = 10000;
+            Ne_Rel_In.BufferMode = MXP.MXP_BUFFERMODE_ENUM.MXP_BUFFERED;
+            Ne_Rel_In.Execute = 0;
+            MXP.MXP_MoveRelativeCmd(110, ref Ne_Rel_In);
+            Thread.Sleep(1000);
+            Ne_Rel_In.Execute = 1;
+            MXP.MXP_MoveRelativeCmd(110, ref Ne_Rel_In);
+            Thread.Sleep(500);
+        }
+        /// <summary>
+        /// Fast Move 는 Pallet를 Out 시킬 때 빠르게 카트리지를 찾아가기 위해서 
+        /// 속도를 빠르게 한 함수임. Rel_NMove 함수나 Rel_PMove 함수는 속도가 느림. 그래서 scan 할때 적절함.
+        /// </summary>
+        /// <param name="index"></param>
+        public void Rel_Fast_Move(int index)
+        {
+            MXP.MXP_MOVERELATIVE_IN Po_Rel_In;
+            Po_Rel_In.Axis.AxisNo = 0;
+            Po_Rel_In.ContinuousUpdate = 0;
+            Po_Rel_In.Distance = position_calibration * index;
+            Po_Rel_In.Velocity = 80;
+            Po_Rel_In.Acceleration = (float)15;
+            Po_Rel_In.Deceleration = (float)15;
+            Po_Rel_In.Jerk = 10000;
+            Po_Rel_In.BufferMode = MXP.MXP_BUFFERMODE_ENUM.MXP_BUFFERED;
+            Po_Rel_In.Execute = 0;
+            MXP.MXP_MoveRelativeCmd(110, ref Po_Rel_In);
+            Thread.Sleep(1000);
+            Po_Rel_In.Execute = 1;
+            MXP.MXP_MoveRelativeCmd(110, ref Po_Rel_In);
+            Thread.Sleep(13500);
+        }
+
+        public void reset()
+        // 축의 이상상태를 해제하고 해당 축의 Error Stop 상태를 StandStill 상태로 리셋한다.
+        // 현재 Position이 초기화 되지 않는다.
+        {
+            MXP.MXP_RESET_OUT Out = new MXP.MXP_RESET_OUT { };
+            MXP.MXP_MOVEABSOLUTE_IN x = new MXP.MXP_MOVEABSOLUTE_IN { };
+            x.Execute = 0;
+            UInt32 i = 0;
+            Motion_Function.MXP_MC_Reset(i, IndexCal((UInt32)MXP_MotionBlockIndex.mcReset) + i, false, Out);
+
+        }
+
+
+        public Single Now_Actual_Position()
+        // 명령된 축의 현재위치를 출력한다. 
+        {
+
+            Single x = Motion_Function.MXP_MC_ReadActualPosition((Int32)0);
+
+            Console.WriteLine(x);
+            return x;
+
+        }
+        public void Exit()
+        {
+            Int32 Status = 0;
+            MXP.MXP_GetKernelStatus(out Status);
+            if (Status >= MXP.MXP_SysStatus.Run)
+            {
+                if (MXP.MXP_SystemStop() == 0)
+                {
+                    MXP.MXP_Destroy();
+                }
+            }
+        }
+
+        public void MXP_Home_Example()
+        {
+            MXP.MXP_HOME_IN In;
+            In.Axis.AxisNo = 0;
+            In.BufferMode = 0; //Not Supported
+            In.Position = 0; //Not Supported
+            In.Execute = 0;
+            MXP.MXP_HomeCmd(20, ref In);
+            In.Execute = 1;
+            MXP.MXP_HomeCmd(20, ref In);
+        }
+    }
+}
---
+++ Dispense Part/Dispense Part/Program.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dispense_Part
+{
+    class Program
+    {
+        /*
+         정렬이 되어 있다고 가정을 한 상태에서 팔레트만 내보내는 과정만 있음.
+             */
+        static void Main(string[] args)
+        {
+
+            Motor motor = new Motor();
+            motor.connect();
+            motor.Servo_Power(false); // false가 항상 모터 on임 
+
+            Out();
+
+        }
+        /*
+         Out 7 : Stopper 
+         
+         Pallet Fisrt Pusher ==> Pallet_Out1(),Pallet_Out5() ==> Pallet Number = 5
+         Pallet Second Pusher ==> Pallet_Out2(),Pallet_Out4() == Pallet Number = 3
+         Pallet Lift ==> Pallet_Out3(),Pallet_Out6() == Pallet Number = 3
+         \
+         Out 3 : 
+             
+             */
+        static public void Out()
+
+        {
+            //Cyl_Stopper_Up();
+            Thread.Sleep(1000);
+            Pallete_Out1();
+            Thread.Sleep(1000);
+            Pallete_Out2();
+            Thread.Sleep(1000);
+            Pallete_Out3();
+            Thread.Sleep(1000);
+            Pallete_Out4();
+            Thread.Sleep(1000);
+            Pallete_Out5();
+            Thread.Sleep(1000);
+            Pallete_Out6();
+            Thread.Sleep(1000);
+            //Cyl_Stopper_Down();
+
+        }
+        static private void Pallete_Out1()
+        {
+            // What is OUT1 ?
+            MXP.MXP_WRITEDIGITALOUTPUT_IN stopper_up_In;
+
+            stopper_up_In.Output.SourceNo = 1;
+            stopper_up_In.OutputNumber = 5;
+            stopper_up_In.Value = 1;
+            stopper_up_In.ExecutionMode = 0;
+            stopper_up_In.Execute = 0;
+            MXP.MXP_WriteDigitalOutputCmd(240, ref stopper_up_In);
+            stopper_up_In.Execute = 1;
+            MXP.MXP_WriteDigitalOutputCmd(240, ref stopper_up_In);
+        }
+
+        static  private void Pallete_Out2()
+        {
+            MXP.MXP_WRITEDIGITALOUTPUT_IN stopper_up_In;
+            stopper_up_In.Output.SourceNo = 1;
+            stopper_up_In.OutputNumber = 3;
+            stopper_up_In.Value = 1;
+            stopper_up_In.ExecutionMode = 0;
+            stopper_up_In.Execute = 0;
+            MXP.MXP_WriteDigitalOutputCmd(240, ref stopper_up_In);
+            stopper_up_In.Execute = 1;
+            MXP.MXP_WriteDigitalOutputCmd(240, ref stopper_up_In);
+        }
+
+        static private void Pallete_Out3()
+        {
+            MXP.MXP_WRITEDIGITALOUTPUT_IN stopper_up_In;
+            stopper_up_In.Output.SourceNo = 1;
+            stopper_up_In.OutputNumber = 1;
+            stopper_up_In.Value = 1;
+            stopper_up_In.ExecutionMode = 0;
+            stopper_up_In.Execute = 0;
+            MXP.MXP_WriteDigitalOutputCmd(240, ref stopper_up_In);
+            stopper_up_In.Execute = 1;
+            MXP.MXP_WriteDigitalOutputCmd(240, ref stopper_up_In);
+        }
+
+        static private void Pallete_Out4()
+        {
+            MXP.MXP_WRITEDIGITALOUTPUT_IN stopper_up_In;
+            stopper_up_In.Output.SourceNo = 1;
+            stopper_up_In.OutputNumber = 3;
+            stopper_up_In.Value = 0;
+            stopper_up_In.ExecutionMode = 0;
+            stopper_up_In.Execute = 0;
+            MXP.MXP_WriteDigitalOutputCmd(240, ref stopper_up_In);
+            stopper_up_In.Execute = 1;
+            MXP.MXP_WriteDigitalOutputCmd(240, ref stopper_up_In);
+        }
+
+        static private void Pallete_Out5()
+        {
+            MXP.MXP_WRITEDIGITALOUTPUT_IN stopper_up_In;
+            stopper_up_In.Output.SourceNo = 1;
+            stopper_up_In.OutputNumber = 5;
+            stopper_up_In.Value = 0;
+            stopper_up_In.ExecutionMode = 0;
+            stopper_up_In.Execute = 0;
+            MXP.MXP_WriteDigitalOutputCmd(240, ref stopper_up_In);
+            stopper_up_In.Execute = 1;
+            MXP.MXP_WriteDigitalOutputCmd(240, ref stopper_up_In);
+        }
+
+        static private void Pallete_Out6()
+        {
+            MXP.MXP_WRITEDIGITALOUTPUT_IN stopper_up_In;
+            stopper_up_In.Output.SourceNo = 1;
+            stopper_up_In.OutputNumber = 1;
+            stopper_up_In.Value = 0;
+            stopper_up_In.ExecutionMode = 0;
+            stopper_up_In.Execute = 0;
+            MXP.MXP_WriteDigitalOutputCmd(240, ref stopper_up_In);
+            stopper_up_In.Execute = 1;
+            MXP.MXP_WriteDigitalOutputCmd(240, ref stopper_up_In);
+        }
+
+        static  private void Cyl_Stopper_Up()
+        {
+            MXP.MXP_WRITEDIGITALOUTPUT_IN stopper_up_In;
+            stopper_up_In.Output.SourceNo = 1;
+            stopper_up_In.OutputNumber = 7;
+            stopper_up_In.Value = 1;
+            stopper_up_In.ExecutionMode = 0;
+            stopper_up_In.Execute = 0;
+            MXP.MXP_WriteDigitalOutputCmd(240, ref stopper_up_In);
+            stopper_up_In.Execute = 1;
+            MXP.MXP_WriteDigitalOutputCmd(240, ref stopper_up_In);
+        }
+        static private void Cyl_Stopper_Down()
+        {
+            MXP.MXP_WRITEDIGITALOUTPUT_IN stopper_down_In;
+            stopper_down_In.Output.SourceNo = 1;
+            stopper_down_In.OutputNumber = 7;
+            stopper_down_In.Value = 0;
+            stopper_down_In.ExecutionMode = 0;
+            stopper_down_In.Execute = 0;
+            MXP.MXP_WriteDigitalOutputCmd(240, ref stopper_down_In);
+            stopper_down_In.Execute = 1;
+            MXP.MXP_WriteDigitalOutputCmd(240, ref stopper_down_In);
+        }
+        static bool test = false;
+        static private void ReadDigitalOutput_On()
+        {
+            MXP.MXP_READDIGITALOUTPUT_IN Read_In;
+            MXP.MXP_READDIGITALOUTPUT_OUT Read_Out;
+            byte Result;
+
+            Read_In.Output.SourceNo = 1;
+            Read_In.OutputNumber = 7;
+
+            Read_In.Enable = 0;
+            MXP.MXP_ReadDigitalOutput(ref Read_In, out Read_Out);
+
+            Result = Read_Out.Value;
+            if (Result == 0)
+            {
+                test = false;
+            }
+        }
+
+        static private void ReadDigitalOutput_Off()
+        {
+            MXP.MXP_READDIGITALOUTPUT_IN Read_In;
+            MXP.MXP_READDIGITALOUTPUT_OUT Read_Out;
+            byte Result;
+
+            Read_In.Output.SourceNo = 1;
+            Read_In.OutputNumber = 7;
+
+            Read_In.Enable = 1;
+            MXP.MXP_ReadDigitalOutput(ref Read_In, out Read_Out);
+
+            Result = Read_Out.Value;
+            if (Result == 1)
+            {
+                test = true;
+            }
+        }
+
+    }
+}
